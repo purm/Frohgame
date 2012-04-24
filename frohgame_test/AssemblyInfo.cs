98b1ffcc9a37ff3682c2a671cc046f8b80d449dd
Purm <purm-dev@secure-mail.biz>
2012-04-25 00:28:56 +0200

    nen paar projekt-einrichtungs-krams

--- frohgame_test/AssemblyInfo.cs
+++ frohgame_test/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Marvin")]
+[assembly: AssemblyCopyright("Purm & Cannap")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
