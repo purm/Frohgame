1dffe3d100b1cf25ffc9050b71145521bbc7842b
qpCH <sh4fty@gmail.com>
2012-04-27 18:54:43 +0200

    Berechnung der Bauzeit funktioniert nun danke an Joern

--- Frohgame/Core/Calculator.cs
+++ Frohgame/Core/Calculator.cs
@@ -35,14 +35,14 @@ namespace Frohgame
 			);
 		}
 		
-		public double CalculateCosts(int element, int NextLevel)
+		public double CalculateCosts (int element, int NextLevel)
 		{
 			ObjectOffset type = Type [element];
 			
 			// Müssen vorher ausgelesen werden!
 			int RoboFabrik = 0; 
 			int NanitFabrik = 0;
-			int UniSpeed = 1;
+			int UniSpeed = 1; // 1 Standard 3 = Speeduni 
 			double Result; // Speicher immer das aktuelle resultat
 			
 			//Berechne die bentötigten Ressourcen Funktioniert!
@@ -51,28 +51,25 @@ namespace Frohgame
 			double Deuterium = Math.Floor (type.Deuterium * Math.Pow (type.Factor, NextLevel - 1));
 			double Energy = Math.Floor (type.Energy * Math.Pow (type.Factor, NextLevel - 1));
 			
-			// Das klappt schonmal 
 			Console.WriteLine ("Benötigte Ressourcen für: " + type.Name + " Metal: " + Metal + " Kristall: " + Crystal + " auf Stufe: " + NextLevel);
 			
 			
 			switch (type.Art) {
 				
 			case "Building":
-			//i = (res * 3600) / (2500 * (Robo -1 * -1) * Math.pow(2, Nanit));
-				//Formel: Zeit in Stunden = ( (Kris+Met) / (2500 * (1 + Stufe Roboterfabrik)) ) * 0,5 ^ Stufe Nani
-				Result = (Metal + Crystal *3600) / (2500 * (RoboFabrik -1 * -1) * Math.Pow (2, NanitFabrik));
-			
-				CalculateTime (Result);
+				Result = ((Metal + Crystal) * 3600) / (2500 * (RoboFabrik - 1 * -1) * Math.Pow (2, NanitFabrik));
+				Result = Result / UniSpeed;
+				CalculateTime(Result);
 				break;
 				
-			case "station":
+			case "Station":
 				
 				break;
 					
-			case "research":
+			case "Research":
 				
 				break;
-			case "fleet": 
+			case "Fleet": 
 				
 				break;
 			default:
@@ -80,57 +77,28 @@ namespace Frohgame
 				break;	
 			}
 			
-			//Result = Result / UniSpeed; //falls uni Speed angeben wurde	
 			return  0;
 		}
 		
-		public double CalculateTime (double Result)
+		public double CalculateTime (double i)
 		{
+			double sec = i % 60;
+			i = Math.Floor (i / 60);        
+			double min = i % 60;
+			i = Math.Floor (i / 60);
+			double hours = i % 24;
+			double days = Math.Floor (i / 24);
 			
-			Result = Math.Max(1, Math.Floor (Result));
+			if (days == 0) { 
 			
-			double sec = Mod(Result, 60);
+			Console.WriteLine (hours + ":" + min + ":" + sec);
 			
-			Result = Div(Result, 60);
-			
-			double min = Mod (Result, 60);
-			
-			Result = Div (Result, 60);
-			
-			double h = Mod (Result, 24);
-			double d = Div (Result, 24);
-			
-			if (sec < 10) {
-				sec = 0 + sec;
 			}
-			if (min < 10) {
-				min = 0 + min;
+			else
+			{
+				Console.WriteLine (days + " Tage " +hours + ":" + min + ":" + sec);
 			}
-			if (h < 10) {
-				h = 0 + h;
-			}
-			if (d == 0) {
-				Console.WriteLine (h + ":" + min + ":" + sec);
-				//  return  h + ':' + min + ':' + sec;
-				
-				return 0;
-			} else {
-				Console.WriteLine (d + "T " + h + ":" + min + ":" + sec);
-				return 0;
-				//  return d+"T "+h+":"+min+":"+sec;
-			}
-		}
-		
-		private double Div (double a, double b)
-		{
-			return Math.Floor (a / b);
-		}
-		
-		private double Mod (double a, double b)
-		{
-			return a - Math.Floor (a / b) * b;
-			
-		
+			return 0;				
 		}
 	}
 } 
