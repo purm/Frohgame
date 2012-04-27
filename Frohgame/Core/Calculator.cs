4e33e060fcba1d97e7eb0ca21cd5d2caef09c7bc
qpCH <sh4fty@gmail.com>
2012-04-27 16:49:56 +0200

    updated calculator

--- Frohgame/Core/Calculator.cs
+++ Frohgame/Core/Calculator.cs
@@ -11,11 +11,9 @@ namespace Frohgame
 		public int Crystal;
 		public int Deuterium;
 		public int Energy;
+		public string Art;
 	}
 	
-	/// <summary>
-	/// Rechnet krams
-	/// </summary>
 	public class Calculator
 	{
 		Dictionary<int, ObjectOffset> Type = new Dictionary<int, ObjectOffset> ();
@@ -32,11 +30,12 @@ namespace Frohgame
 					Crystal = 15, 
 				    Deuterium = 0,
 				    Energy = 0,
+				    Art = "Building",
 				}
 			);
 		}
-			
-		public int CalculateCosts (int element, int NextLevel)
+		
+		public double CalculateCosts(int element, int NextLevel)
 		{
 			ObjectOffset type = Type [element];
 			
@@ -44,43 +43,74 @@ namespace Frohgame
 			int RoboFabrik = 0; 
 			int NanitFabrik = 0;
 			int UniSpeed = 1;
+			double Result; // Speicher immer das aktuelle resultat
 			
-	        //Berechne die bentötigten Ressourcen Funktioniert!
+			//Berechne die bentötigten Ressourcen Funktioniert!
 			double Metal = Math.Floor (type.Metal * Math.Pow (type.Factor, NextLevel - 1));
 			double Crystal = Math.Floor (type.Crystal * Math.Pow (type.Factor, NextLevel - 1));
 			double Deuterium = Math.Floor (type.Deuterium * Math.Pow (type.Factor, NextLevel - 1));
 			double Energy = Math.Floor (type.Energy * Math.Pow (type.Factor, NextLevel - 1));
 			
 			// Das klappt schonmal 
-			Console.WriteLine ("Benötigte Ressourcen für: " + type.Name + " Metal: " + Metal + " Kristall: " + Crystal + " auf Stufe: " + NextLevel );
+			Console.WriteLine ("Benötigte Ressourcen für: " + type.Name + " Metal: " + Metal + " Kristall: " + Crystal + " auf Stufe: " + NextLevel);
 			
-			//Einfach SupplyBuilding Berechnung //TODO Irgendwas ist falsch
-			double Formel1 = (type.Metal + type.Crystal * 3600) / (2500 * RoboFabrik - 1 * -1) * Math.Pow (2, NanitFabrik);
-			if(UniSpeed > 1) Formel1 = Formel1 / UniSpeed; //falls uni Speed angeben wurde
 			
-			Formel1 = Math.Max (1, Math.Floor (Formel1));
-			double sec = Mod (Formel1, 60);
+			switch (type.Art) {
+				
+			case "Building":
+			//i = (res * 3600) / (2500 * (Robo -1 * -1) * Math.pow(2, Nanit));
+				//Formel: Zeit in Stunden = ( (Kris+Met) / (2500 * (1 + Stufe Roboterfabrik)) ) * 0,5 ^ Stufe Nani
+				Result = (Metal + Crystal *3600) / (2500 * (RoboFabrik -1 * -1) * Math.Pow (2, NanitFabrik));
 			
-			Formel1 = Div (Formel1, 60);
+				CalculateTime (Result);
+				break;
+				
+			case "station":
+				
+				break;
+					
+			case "research":
+				
+				break;
+			case "fleet": 
+				
+				break;
+			default:
+				
+				break;	
+			}
 			
-			double min = Mod (Formel1, 60);
+			//Result = Result / UniSpeed; //falls uni Speed angeben wurde	
+			return  0;
+		}
+		
+		public double CalculateTime (double Result)
+		{
 			
-			Formel1 = Div (Formel1, 60);
+			Result = Math.Max(1, Math.Floor (Result));
 			
-			double h = Mod (Formel1, 24);
-			double d = Div (Formel1, 24);
+			double sec = Mod(Result, 60);
+			
+			Result = Div(Result, 60);
+			
+			double min = Mod (Result, 60);
+			
+			Result = Div (Result, 60);
+			
+			double h = Mod (Result, 24);
+			double d = Div (Result, 24);
 			
 			if (sec < 10) {
-				sec = '0' + sec;
+				sec = 0 + sec;
 			}
 			if (min < 10) {
-				min = '0' + min;
+				min = 0 + min;
 			}
 			if (h < 10) {
-				h = '0' + h;
+				h = 0 + h;
 			}
 			if (d == 0) {
-				Console.WriteLine (h + ':' + min + ':' + sec);
+				Console.WriteLine (h + ":" + min + ":" + sec);
 				//  return  h + ':' + min + ':' + sec;
 				
 				return 0;
